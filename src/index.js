9b16e202df1125465f59cf2c9c8fa175af4a5e2c
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-01-30 21:02:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Correct import statement
+import ReactDOM from 'react-dom'; // Correct import statement hello
 import App from './App';
 import './index.css';
 
